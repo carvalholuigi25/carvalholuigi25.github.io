6f904388e923bcdaaa8cb533f1c2ecd3dd4d2290
Luigi Carvalho <carvalholuigi25@gmail.com>
2022-12-10 20:26:15 +0000

    fixed the search bar functions

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,19 @@ async function getData() {
 function getMyStatusBar(mydata, i) {
       var statusbar = "";
 	
-      if(mydata[i].value >= 0 && mydata[i].value < 50) {
-	statusbar = "bg-danger";
-      } else if(mydata[i].value >= 50 && mydata[i].value < 70) {
-	statusbar = "bg-warning";      
-      } else if(mydata[i].value >= 70 && mydata[i].value < 80) {
-	statusbar = "bg-info";      
-      } else {
-	statusbar = "bg-success";      
-      }	
+      if(mydata != null && mydata.length > 0) {
+			if(mydata[i].value >= 0 && mydata[i].value < 50) {
+				statusbar = "bg-danger";
+			} else if(mydata[i].value >= 50 && mydata[i].value < 70) {
+				statusbar = "bg-warning";      
+			} else if(mydata[i].value >= 70 && mydata[i].value < 80) {
+				statusbar = "bg-info";      
+			} else {
+				statusbar = "bg-success";      
+			}
+	  }	else {
+		statusbar = "bg-info";
+	  }
 	
       return statusbar;
 }
@@ -71,80 +75,194 @@ function getMySearchBar(enableFilter = true) {
 		</form>`;
 
 		if(document.querySelector('#frmsrchdata')) {
+			var jspobjsrch = localStorage.getItem("searchObj") ? JSON.parse(localStorage.getItem("searchObj")) : null;
+			var srchval = jspobjsrch ? jspobjsrch.search : (document.querySelector('#searchinp').value ?? "");
+			var orderbyval = jspobjsrch ? jspobjsrch.orderby : (document.querySelector("#orderinp").value ?? "asc");
+			var filterbyval = jspobjsrch ? jspobjsrch.filterby : (document.querySelector("#filterbynameinp").value ?? "id");
+
 			document.querySelector('#frmsrchdata').onsubmit = function(e) {
 				e.preventDefault();
 			};
 
+			if(localStorage.getItem("searchObj")) {
+				document.querySelector('#orderinp option').removeAttribute('selected');
+				document.querySelector('#filterbynameinp option').removeAttribute('selected');
+				document.querySelector('#searchinp').value = srchval;
+				
+				if(document.querySelectorAll('#orderinp option[value="'+orderbyval+'"]')[0]) {
+					document.querySelectorAll('#orderinp option[value="'+orderbyval+'"]')[0].setAttribute('selected', 'selected');
+				}
+
+				if(document.querySelectorAll('#filterbynameinp option[value="'+filterbyval+'"]')[0]) {
+					document.querySelectorAll('#filterbynameinp option[value="'+filterbyval+'"]')[0].setAttribute('selected', 'selected');
+				}
+			}
+
 			if(document.querySelector('#searchinp')) {
-				document.querySelector('#searchinp').oninput = function(e) {
+				document.querySelector('#searchinp').oninput = async function(e) {
 					e.preventDefault();
+					srchval = this.value;
+					getResFilter(srchval, orderbyval, filterbyval);
+					await getMySkillsData();
 				};
 			}
 
 			if(document.querySelector('#orderinp')) {
-				document.querySelector('#orderinp').onchange = function(e) {
+				document.querySelector('#orderinp').onchange = async function(e) {
 					e.preventDefault();
-					document.querySelector('#orderinp option').removeAttribute('selected');
+					
+					for(var i = 0; i < document.querySelectorAll('#orderinp option').length; i++) {
+						document.querySelectorAll('#orderinp option')[i].removeAttribute('selected');
+					}
+
 					document.querySelectorAll('#orderinp option[value="'+this.value+'"]')[0].setAttribute('selected', 'selected');
+					orderbyval = this.value;
+					getResFilter(srchval, orderbyval, filterbyval);
+					await getMySkillsData();
 				};
 			}
 
 			if(document.querySelector('#filterbynameinp')) {
-				document.querySelector('#filterbynameinp').onchange = function(e) {
+				document.querySelector('#filterbynameinp').onchange = async function(e) {
 					e.preventDefault();
-					document.querySelector('#filterbynameinp option').removeAttribute('selected');
+					
+					for(var i = 0; i < document.querySelectorAll('#filterbynameinp option').length; i++) {
+						document.querySelectorAll('#filterbynameinp option')[i].removeAttribute('selected');
+					}
+					
 					document.querySelectorAll('#filterbynameinp option[value="'+this.value+'"]')[0].setAttribute('selected', 'selected');
+					filterbyval = this.value;
+					getResFilter(srchval, orderbyval, filterbyval);
+					await getMySkillsData();
 				};
 			}
 
 			if(document.querySelector('#searchclear')) {
-				document.querySelector('#searchclear').onclick = function(e) {
+				document.querySelector('#searchclear').onclick = async function(e) {
 					e.preventDefault();
-				
 					document.querySelector('#searchinp').value = "";
-					document.querySelector('#orderinp option').removeAttribute('selected');
-					document.querySelectorAll('#orderinp option')[0].setAttribute('selected', 'selected');
-					document.querySelector('#filterbynameinp option').removeAttribute('selected');
-					document.querySelectorAll('#filterbynameinp option')[0].setAttribute('selected', 'selected');
-					
-					localStorage.setItem("searchObj",  JSON.stringify({
-						search: document.querySelectorAll('#searchinp')[0].value,
-						orderby: document.querySelectorAll('#orderinp option')[0].value,
-						filterby: document.querySelectorAll('#filterbynameinp option')[0].value
-					}));
 
-					console.log(localStorage.getItem("searchObj"));
+					for(var i = 0; i < document.querySelectorAll('#orderinp option').length; i++) {
+						document.querySelectorAll('#orderinp option')[i].removeAttribute('selected');
+					}
+
+					for(var j = 0; j < document.querySelectorAll('#filterbynameinp option').length; j++) {
+						document.querySelectorAll('#filterbynameinp option')[j].removeAttribute('selected');
+					}
+
+					document.querySelectorAll('#orderinp option[value="'+jspobjsrch.orderby+'"]')[0].setAttribute('selected', 'selected');
+					document.querySelectorAll('#filterbynameinp option[value="'+jspobjsrch.filterby+'"]')[0].setAttribute('selected', 'selected');
+					
+					srchval = "";
+					orderbyval = document.querySelectorAll('#orderinp option[selected="selected"]')[0].value;
+					filterbyval = document.querySelectorAll('#filterbynameinp option[selected="selected"]')[0].value;
+					getResFilter(srchval, orderbyval, filterbyval);
+					await getMySkillsData();
 				};
 			}
 
 			if(document.querySelector('#searchsub')) {
-				document.querySelector('#searchsub').onclick = function(e) {
+				document.querySelector('#searchsub').onclick = async function(e) {
 					e.preventDefault();
-					var valsrchinp = document.querySelector('#searchinp').value;
-					var valfnameinp = document.querySelector('#filterbynameinp option[selected="selected"]').value;
-					var valorderbyinp = document.querySelector('#orderinp option[selected="selected"]').value;
-
-					var objsrch = {
-						search: valsrchinp,
-						orderby: valorderbyinp,
-						filterby: valfnameinp
-					};
-
-					localStorage.setItem("searchObj",  JSON.stringify(objsrch));
-					console.log(localStorage.getItem("searchObj"));
+					getResFilter(srchval, orderbyval, filterbyval);
+					await getMySkillsData();
 				};
 			}
 		}
 	}
 }
 
+function getResFilter(searchval, orderbyval, filterbyval) {
+	var valsrchinp = searchval ?? document.querySelector('#searchinp').value;
+	var valorderbyinp = orderbyval ?? document.querySelector('#orderinp option[selected="selected"]').value;
+	var valfnameinp = filterbyval ?? document.querySelector('#filterbynameinp option[selected="selected"]').value;
+
+	var objsrch = {
+		search: valsrchinp,
+		orderby: valorderbyinp,
+		filterby: valfnameinp
+	};
+
+	localStorage.setItem("searchObj",  JSON.stringify(objsrch));
+}
+
+function doFilterSearch(objsrch, mydata) {
+	if(objsrch != null) {
+		if(objsrch.search != "") {
+			if(objsrch.filterby == "id") 
+			{
+				mydata = mydata.filter(x => x.id == objsrch.search);
+			}
+			else if(objsrch.filterby == "value") 
+			{
+			    mydata = mydata.filter(x => x.value == parseInt(objsrch.search));
+			}
+			else if(objsrch.filterby == "name") 
+			{
+				mydata = mydata.filter(x => x.name.toLowerCase().includes(objsrch.search.toLowerCase()));
+			}
+			else 
+			{
+				mydata = mydata.filter(x => x.name.toLowerCase().includes(objsrch.search.toLowerCase()));
+			}
+		}
+
+		if(objsrch.orderby != "") {
+			mydata = mydata.sort((x, y) => {
+				var valx = "";  var valy = "";
+				
+				if(objsrch.filterby == "name") {
+					valx = x.name.toLowerCase();
+					valy = y.name.toLowerCase();
+				} else if(objsrch.filterby == "value") {
+					valx = x.value;
+					valy = y.value;
+				} else if(objsrch.filterby == "id") {
+					valx = x.id;
+					valy = y.id;
+				} else {
+					valx = x.id;
+					valy = y.id;
+				}
+
+				if(objsrch.orderby == "asc") {
+					if(valx < valy) {
+						return -1;
+					}
+	
+					if(valx > valy) {
+						return 1;
+					}
+				} else {
+					if(valx > valy) {
+						return -1;
+					}
+	
+					if(valx < valy) {
+						return 1;
+					}
+				}
+
+				return 0;
+			});
+		}
+	}
+
+	return mydata;
+}
+
 async function getMySkillsData() {
    if(document.querySelectorAll("#myskills")[0]) {
        var content = ""; var datacnt = ""; var statusbar = ""; var mydata = "";
        var isTable = true;
+	   var objsrch = localStorage.getItem("searchObj") != null ? JSON.parse(localStorage.getItem("searchObj")) : null;
 	   
        await getData().then((data) => {
 	  mydata = data.myskills ?? data;
+
+	  if(objsrch != null) {
+		  mydata = doFilterSearch(objsrch, mydata);
+	  }
 	  
 	  if(mydata != null && mydata.length > 0) {
 	   for(var i = 0; i < mydata.length; i++) {
@@ -152,23 +270,24 @@ async function getMySkillsData() {
 
 	      if(isTable) {
 	        datacnt += `
-		<tr>
-		  <td>${mydata[i].name}</td>
-		  <td>
-		   <div class="progress">
-		      <div class="progress-bar progress-bar-striped progress-bar-animated ${statusbar}" role="progressbar" 
-			aria-label="${mydata[i].name}" aria-valuenow="${mydata[i].value}" aria-valuemin="0" aria-valuemax="100" 
-			style="width: ${mydata[i].value}%">
-			${mydata[i].value}%
-		      </div>
-		   </div>
-		  </td>
+		  <tr>
+			<td>${mydata[i].id}</td>
+			<td>${mydata[i].name}</td>
+			<td>
+			<div class="progress">
+				<div class="progress-bar progress-bar-striped progress-bar-animated ${statusbar}" role="progressbar" 
+				aria-label="${mydata[i].name}" aria-valuenow="${mydata[i].value}" aria-valuemin="0" aria-valuemax="100" 
+				style="width: ${mydata[i].value}%">
+				${mydata[i].value}%
+				</div>
+			</div>
+			</td>
 	       </tr>`;
 	      } else {
 		datacnt += `
 	        <div class="pbar pbar${i+1}" id="pbar${i+1}">
 		 <div class="subpbar">
-		   <label class="title">${mydata[i].name}</label>
+		   <label class="title">${mydata[i].name} (Id: ${mydata[i].id})</label>
 		   <div class="progress">
 		      <div class="progress-bar progress-bar-striped progress-bar-animated ${statusbar}" role="progressbar" 
 			aria-label="${mydata[i].name}" aria-valuenow="${mydata[i].value}" aria-valuemin="0" aria-valuemax="100" 
@@ -185,20 +304,27 @@ async function getMySkillsData() {
 	  }
 	       
 	  if(isTable) {
-	     content = `
-	     <div class="table-responsive mpbartbl" id="mpbartbl">
-		  <table class="table table-bordered pbartbl" id="pbartbl">
-		    <thead class="pbartblthead" id="pbartblthead">
-		     <tr>
-			<th>Name</th>
-			<th>Progress Value</th>
-		     </tr>
-		   </thead>
-		   <tbody class="pbartbltbody" id="pbartbltbody">
-		    ${datacnt}
-		   </tbody>
-		 </table>
-	     </div>`;  
+		if(mydata.length > 0) {
+			content = `
+			<div class="table-responsive mpbartbl" id="mpbartbl">
+			 <table class="table table-bordered pbartbl" id="pbartbl">
+			   <thead class="pbartblthead" id="pbartblthead">
+				<tr>
+			   <th>Id</th>
+			   <th>Name</th>
+			   <th>Progress Value</th>
+				</tr>
+			  </thead>
+			  <tbody class="pbartbltbody" id="pbartbltbody">
+			   ${datacnt}
+			  </tbody>
+			</table>
+			</div>`;  
+		}
+		else 
+		{
+			content = datacnt;
+		}
 	  } else {
 	    content = datacnt;	  
 	  }
@@ -236,34 +362,36 @@ function getMyPagination(ary, len, page = 1) {
 	  pagnavlinks += `<li class="page-item"><button type="button" class="page-link btnitem btnitem${i} ${active}" id="btnitem${i}">${i+1}</button></li>`;
 	}
 	    
-	content = `
-	<nav class="spagination">
-	  <ul class="pagination">
-	    <li class="page-item">
-	     <button type="button" class="page-link btnfirst" id="btnfirst" aria-label="First">
-	      <i class="bi bi-chevron-double-left"></i>
-	     </button>
-	    </li>
-	    <li class="page-item">
-	     <button type="button" class="page-link btnprev" id="btnprev" aria-label="Previous">
-	      <i class="bi bi-chevron-left"></i>
-	     </button>
-	    </li>
-	    ${pagnavlinks}
-	    <li class="page-item">
-	      <button type="button" class="page-link btnnext" id="btnnext" aria-label="Next">
-	      	<i class="bi bi-chevron-right"></i>
-	      </button>
-	    </li>
-	    <li class="page-item">
-	      <button type="button" class="page-link btnlast" id="btnlast" aria-label="Last">
-	      	<i class="bi bi-chevron-double-right"></i>
-	      </button>
-	    </li>
-	  </ul>
-	</nav>
-        <span class="d-block mt-3 mb-3 page" id="page"></span>`;
-	    
+	if(numPages(len) > 0) {
+		content = `
+		<nav class="spagination">
+		  <ul class="pagination">
+			<li class="page-item">
+			 <button type="button" class="page-link btnfirst" id="btnfirst" aria-label="First">
+			  <i class="bi bi-chevron-double-left"></i>
+			 </button>
+			</li>
+			<li class="page-item">
+			 <button type="button" class="page-link btnprev" id="btnprev" aria-label="Previous">
+			  <i class="bi bi-chevron-left"></i>
+			 </button>
+			</li>
+			${pagnavlinks}
+			<li class="page-item">
+			  <button type="button" class="page-link btnnext" id="btnnext" aria-label="Next">
+				  <i class="bi bi-chevron-right"></i>
+			  </button>
+			</li>
+			<li class="page-item">
+			  <button type="button" class="page-link btnlast" id="btnlast" aria-label="Last">
+				  <i class="bi bi-chevron-double-right"></i>
+			  </button>
+			</li>
+		  </ul>
+		</nav>
+		<span class="d-block mt-3 mb-3 page" id="page"></span>`;
+	}
+
 	document.querySelector('#mypagination').innerHTML = content;
 	    
 	if(document.querySelector('#mypagination')) {
@@ -325,24 +453,29 @@ function doPagination(ary, len, page = 1) {
     if (page < 1) page = 1;
     if (page > numPages(len)) page = numPages(len);
 
-    pbartbltbody.innerHTML = "";
+	if(pbartbltbody) {
+		pbartbltbody.innerHTML = "";
+	}
 
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < len; i++) {
-	statusbar = getMyStatusBar(ary, i);
-	pbartbltbody.innerHTML += `
-	<tr>
-	  <td>${ary[i].name}</td>
-	  <td>
-	   <div class="progress">
-	      <div class="progress-bar progress-bar-striped progress-bar-animated ${statusbar}" role="progressbar" 
-		aria-label="${ary[i].name}" aria-valuenow="${ary[i].value}" aria-valuemin="0" aria-valuemax="100" 
-		style="width: ${ary[i].value}%">
-		${ary[i].value}%
-	      </div>
-	   </div>
-	  </td>
-        </tr>`;
-    }
+    if(ary.length > 0) {
+		for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < len; i++) {
+			statusbar = getMyStatusBar(ary, i);
+			pbartbltbody.innerHTML += `
+			<tr>
+			  <td>${ary[i].id}</td>
+			  <td>${ary[i].name}</td>
+			  <td>
+			   <div class="progress">
+				  <div class="progress-bar progress-bar-striped progress-bar-animated ${statusbar}" role="progressbar" 
+				aria-label="${ary[i].name}" aria-valuenow="${ary[i].value}" aria-valuemin="0" aria-valuemax="100" 
+				style="width: ${ary[i].value}%">
+				${ary[i].value}%
+				  </div>
+			   </div>
+			  </td>
+			</tr>`;
+		}
+	}
 	
     if(page_span) {
     	page_span.innerHTML = page + "/" + numPages(len);
@@ -488,7 +621,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     navClickLink();
-	getMySearchBar(false);
+	getMySearchBar(true);
     await getMySkillsData();
 
     window.addEventListener('scroll', () => {
